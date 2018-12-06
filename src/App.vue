a73211330f824b1c8fa2d9437c20dd7631bcfd97
Alex <susl1k123@mail.ru>
2018-12-06 03:01:31 +0300

    parent=>child info transfer

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ carName }}</h1>
+
+        <app-car
+                :carName="carNamePar"
+                :carYear="parentCarYear">
+        </app-car>
+    </div>
+</template>
+
+<script>
+    import Car from './Car.vue'
+
+    export default {
+        data() {
+            return {
+                carNamePar: 'TESLA from parent',
+                parentCarYear: "2099"
+            }
+        },
+        components: {
+            appCar: Car
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/Car.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="car">
+        <h3>Name: {{ carName }}</h3>
+        <p>Year: {{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: ['carName', 'carYear'],
+        data() {
+            return {
+                //carName: 'Tesla',
+                //carYear: '2018'
+            }
+        }
+    }
+
+</script>
+
+<style>
+    .car {
+        border: 1px dotted cornflowerblue;
+        padding: 5px;
+    }
+</style>
